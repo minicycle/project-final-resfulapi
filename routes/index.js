06db7619d31b57aa7f7bb8c04b5ff0d49ef6a287
Thanarat Pongjan <siratat59@gmail.com>
2023-03-03 15:27:03 +0700

    indexRouter

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,6 @@ var express = require('express');
 const { route } = require('./cars');
 var router = express.Router();
 
-router.get('/', (req,res,next)=>{res.json({message:'Thanarat Pongjan'})});
+router.get('/', (req,res,next)=>{res.json({message:'Welcome to Project API'})});
 
 module.exports = router;
